0b1705303c4ce764dfd8eeec42aab3983aa1ba36
Fran Blažević <fran.blazevic0@gmail.com>
2026-03-10 02:34:45 +0100

    Update actions.ts
    Fix data fetching

--- app/actions.ts
+++ app/actions.ts
@@ -1,7 +1,7 @@
 import { Song } from "@/lib/sorter";
 
 export async function fetchSongs(): Promise<Song[]> {
-    const data = await fetch(`https://raw.githubusercontent.com/fran-bl/esc-song-data/refs/heads/main/songs.json?t=${Date.now()}`)
+    const data = await fetch("https://raw.githubusercontent.com/fran-bl/esc-song-data/main/songs.json", { cache: "no-store" })
     const songs = await data.json()
 
     return songs.filter((song: Song) => song.title != null)
